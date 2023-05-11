f392d6338909d5ec5f852d50440c59de3494342f
Salvatore De Paola <salvatoredepaola8@gmail.com>
2023-05-11 22:57:41 +0200

    layout completo

--- js/main.js
+++ js/main.js
@@ -4,64 +4,56 @@ createApp({
         data() {
             return {
                 search: "",
+                messaggio: "",
                 contatti: [
                     {
                         image: 'img/avatar_1.jpg',
                         nome: 'Michele',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
                         ultimoAccesso: '12:00',
-                        ishovering: false,
-
                     },
                     {
                         image: 'img/avatar_2.jpg',
                         nome: 'fabio',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
-                        ultimoAccesso: '12:00',
-                        ishovering: false,
+                        ultimoAccesso: '12:00', 
 
                     },
                     {
                         image: 'img/avatar_3.jpg',
                         nome: 'Samuele',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
-                        ultimoAccesso: '12:00',
-                        ishovering: false,
+                        ultimoAccesso: '12:00', 
                     },
                     {
                         image: 'img/avatar_4.jpg',
                         nome: 'Alessandro B.',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
-                        ultimoAccesso: '12:00',
-                        ishovering: false,
+                        ultimoAccesso: '12:00', 
                     },
                     {
                         image: 'img/avatar_5.jpg',
                         nome: 'lessandro L.',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
-                        ultimoAccesso: '12:00',
-                        ishovering: false,
+                        ultimoAccesso: '12:00', 
                     },
                     {
                         image: 'img/avatar_6.jpg',
                         nome: 'Claudia',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
-                        ultimoAccesso: '12:00',
-                        ishovering: false,
+                        ultimoAccesso: '12:00', 
                     },
                     {
                         image: 'img/avatar_7.jpg',
                         nome: 'Federico',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
-                        ultimoAccesso: '12:00',
-                        ishovering: false,
+                        ultimoAccesso: '12:00', 
                     },
                     {
                         image: 'img/avatar_8.jpg',
                         nome: 'Davide',
                         ultimoMessaggio: 'Ultimo messaggio inviato',
-                        ultimoAccesso: '12:00',
-                        ishovering: false,
+                        ultimoAccesso: '12:00', 
                     },
                 ]
         }
@@ -69,6 +61,7 @@ createApp({
     methods: {
         testovuoto() {
             this.search = "";
+            this.messaggio = "";
         },
     }
 }).mount('#app')
